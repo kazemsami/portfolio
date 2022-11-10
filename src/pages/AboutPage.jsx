0fecde91e8b4a5728cfe0eb950ea4873383b9f6d
Kazem Abusitta <kabusitt@lab1r3s5.42abudhabi.ae>
2022-11-10 05:39:18 +0400

    changed some information in the about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -11,13 +11,18 @@ const AboutPage = () => {
           <Typography variant="h4" component="div">
             Kazem Abousetta
           </Typography>
-          <Divider sx={{m: 2, background: "gray"}}/>
+          <Divider sx={{ m: 2, background: "gray" }} />
           <Typography variant="body1">
             A passionate coder who is very enthusiastic and always looking
-            forward to learning new technologies. My interests are gaming and
-            coding. 4 years of experience in multiple programming languges and
-            frameworks including C++, C, HTML, CSS, JavaScript and ReactJS. I am
-            currently looking for a position as a software developer.
+            forward to learning new technologies. <br />
+            <br /> I am currently a student at 42 Abu Dhabi coding school and I have been studying there since December 2020. I
+            heard about programming at the age of 21 and that is when I started learning about different programming languages. My hobbies are gaming, music and
+            coding.
+            <br />
+            <br />I have 4 years of experience in multiple programming languges
+            and frameworks including C++, C, HTML, CSS, JavaScript and ReactJS.
+            The fields that I am interested in are Software Development, Cyber
+            Security and Artificial Intelligence.
           </Typography>
         </CardContent>
       </StyledCard>
@@ -27,14 +32,12 @@ const AboutPage = () => {
 
 const StyledCard = styled(Card)`
   padding: 100px;
-  @media (max-width: 768px)
-  {
-    padding:30px;
+  @media (max-width: 768px) {
+    padding: 30px;
   }
-  @media (max-width: 468px)
-  {
+  @media (max-width: 468px) {
     padding: 10px;
   }
-`
+`;
 
 export default AboutPage;
